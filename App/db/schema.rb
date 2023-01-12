dfaaebc6ed40851e8b0af6bbc8e6f2eae5c7091e
nomanalimanj <crnomanali@gmail.com>
2023-01-12 21:04:41 +0500

    Migration Practice Code with migration Files

--- App/db/schema.rb
+++ App/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_12_145146) do
+ActiveRecord::Schema.define(version: 2023_01_12_160038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2023_01_12_145146) do
   end
 
   create_table "blogs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "chats", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
