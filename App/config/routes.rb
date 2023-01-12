901a026055e1357d779aa3ae60fcc0969d1249e8
nomanalimanj <crnomanali@gmail.com>
2023-01-12 20:23:12 +0500

    Product Related Migration

--- App/config/routes.rb
+++ App/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   resources :products
   resources :blogs
+  
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
